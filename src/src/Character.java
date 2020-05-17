fda8ab235b5ef6a3deb04fd1430094468b2f169e
juanpablodelgreco <juanpablo.delgreco@hotmail.com>
2020-05-17 16:51:00 -0300

    Fix mover
    Se preguntaba si era vacio el obstaculo, ahora se pregunta por null.
    Ya es posible pasar de muelle a taberna y viceversa.

--- src/src/Character.java
+++ src/src/Character.java
@@ -15,19 +15,19 @@ public class Character {
 		this.partida = j;
 	}
 
-	//Retorna un String con la localizacion y los items al alcance del personaje.
+	// Retorna un String con la localizacion y los items al alcance del personaje.
 	public String mirar() {
 		return "Estas en un " + posicion.getName() + ". " + "En el suelo hay " + posicion.listarItems() + ". "
 				+ "Hay un " + posicion.listarNpcs() + posicion.listarConexiones() + ". ";
 	}
 
-	//Retorna el nombre del personaje.
+	// Retorna el nombre del personaje.
 	public String name() {
 		return "Tu nombre es " + name;
 	}
 
-	//Retorna un String con tu inventario actual.
-	//(Falta arreglar la salida)
+	// Retorna un String con tu inventario actual.
+	// (Falta arreglar la salida)
 	public String inventario() {
 
 		if (inventario.isEmpty())
@@ -47,19 +47,21 @@ public class Character {
 	public String mover(String direccion) {
 		Connection c = hayConexion(direccion);
 		if (c != null) {
-			if (!c.getObstacle().isEmpty() && existeNPC(c.getObstacle())) {
+			if (c.getObstacle() != null && existeNPC(c.getObstacle())) {
 				NPC np;
 				np = buscarNPC(c.getObstacle());
 				return np.getDescription();
 			} else {
 				posicion = buscarLocalizacion(c.getLocation());
+				System.out.println(posicion.getName());
 				return "Usted esta en " + posicion.getName();
 			}
 		} else
 			return "No es posible moverse a esa posicion";
 	}
-	
-	//Si este personaje se encuentra en la localizacion del personaje retorna su talk.
+
+	// Si este personaje se encuentra en la localizacion del personaje retorna su
+	// talk.
 	public String hablarCon(String name) {
 		if (existeNPC(name)) {
 			NPC npc = buscarNPC(name);
@@ -70,14 +72,16 @@ public class Character {
 		return "No existe el personaje con quien queres hablar.";
 	}
 
-	//Agrega al inventario el item si este se encuentra en la localizacion del personaje.
+	// Agrega al inventario el item si este se encuentra en la localizacion del
+	// personaje.
 	public void agarrarItem(String item) {
 		for (String i : posicion.getItems())
 			if (i.equals(item))
 				inventario.add(i);
 	}
 
-	//Retorna una conexion si en esa direccion indicada se puede ir hacia algun lado.
+	// Retorna una conexion si en esa direccion indicada se puede ir hacia algun
+	// lado.
 	private Connection hayConexion(String direccion) {
 		for (Connection c : posicion.getConnections())
 			if (c.getDirection().equals(direccion))
@@ -86,14 +90,15 @@ public class Character {
 		return null;
 	}
 
-	//Retorna la localizacion si esta existe en la partida.
+	// Retorna la localizacion si esta existe en la partida.
 	private Location buscarLocalizacion(String name) {
 		for (Location l : partida.getLocations())
 			if (l.getName().equals(name))
 				return l;
 		return null;
 	}
-	//Retorna el NPC buscado si este se encuentra en la partida.
+
+	// Retorna el NPC buscado si este se encuentra en la partida.
 	private NPC buscarNPC(String npc) {
 		if (existeNPC(npc)) {
 			for (NPC np : partida.getNpcs()) {
@@ -103,7 +108,8 @@ public class Character {
 		}
 		return null;
 	}
-	//Retorna true si el NPC existe en la posicion actual del personaje.
+
+	// Retorna true si el NPC existe en la posicion actual del personaje.
 	private boolean existeNPC(String name) {
 		for (String i : posicion.getNpcs()) {
 			if (i.equals(name))
@@ -111,15 +117,21 @@ public class Character {
 		}
 		return false;
 	}
-	//Deberia poder usar un elemento del inventario contra un NPC y ver si le hace efecto o no.
-	//Si logra eliminarlo al NPC con este elemento deberia retornar el on_trigger.
-	//Sino deberia retornar una leyenda de "Eso no ha servido de nada".
-//	public void usarEn(String elemento, String npc) {
-//		if (existeNPC(npc)) {
-//			posicion.getNpcs().remove(0);
+
+	// Deberia poder usar un elemento del inventario contra un NPC y ver si le hace
+	// efecto o no.
+	// Si logra eliminarlo al NPC con este elemento deberia retornar el on_trigger.
+	// Sino deberia retornar una leyenda de "Eso no ha servido de nada".
+	public void usarEn(String elemento, String npc) {
+		if (existeNPC(npc)) {
+			posicion.getNpcs().remove(0);
 //			System.out.println(posicion.getNpcs());
 //			System.out.println("->" + buscarNPC(npc));
-//		}
-//
-//	}
+		}
+	}
+
+	public Location getPosicion() {
+		return posicion;
+	}
+
 }
