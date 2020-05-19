da0c646a71d21742b9c18eb658c11710678777f3
Emanuel Cardozo <leuname.01@gmail.com>
2020-05-19 16:35:15 -0300

    Cambiando el inventory

--- src/src/Inventory.java
+++ src/src/Inventory.java
@@ -1,8 +1,24 @@
 package src;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import entities.Item;
 
 public class Inventory {
-	private String description;
-	private ArrayList<Item> items;
-}	
+	private Map<String, Item> items;
+
+	public Inventory() {
+		super();
+		this.items = new HashMap<String, Item>();
+	}
+
+	public void agregarItem(Item item) {
+		items.put(item.getName(), item);
+	}
+
+	public boolean isEmpty() {
+		return items.isEmpty();
+	}
+
+}
