3699b754577334bd2ad13739469f9b58332e7e93
Emanuel Cardozo <leuname.01@gmail.com>
2020-05-19 12:30:06 -0300

    Nuevo Action - constructor, getter and setter

--- src/entities/Action.java
+++ src/entities/Action.java
@@ -2,4 +2,16 @@ package entities;
 
 public class Action {
 	private String name;
+		
+	public Action(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
