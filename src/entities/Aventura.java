da69a40fdd5d919dd5972529845b7b39cd1f7914
Emanuel Cardozo <leuname.01@gmail.com>
2020-05-19 16:35:43 -0300

    Creando la clase aventura

---
+++ src/entities/Aventura.java
@@ -0,0 +1,16 @@
+package entities;
+
+import java.util.ArrayList;
+
+import src.EndGame;
+
+public class Aventura {
+
+	private Mapa mundo;
+	private ArrayList<EndGame> finales;
+
+	public Aventura(String path) {
+
+	}
+
+}
