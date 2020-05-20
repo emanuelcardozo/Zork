97b753263e162639d3959d8fa9828558f1d32f0a
Emanuel Cardozo <leuname.01@gmail.com>
2020-05-20 14:45:45 -0300

    Implementando una clase Mundo (mapa)

--- src/entities/Mundo.java
+++ src/entities/Mundo.java
@@ -2,18 +2,27 @@ package entities;
 
 import java.util.Map;
 
-import entities.Location;
+public class Mundo {
 
-public class Mapa {
-
-	private Character jugador;
 	private String inicio;
 	private Map<String, Location> locations;
 
-	public Mapa(Character jugador, String inicio, Map<String, Location> locations) {
+	public Mundo(String inicio, Map<String, Location> locations) {
 		super();
-		this.jugador = jugador;
+		this.inicio = inicio;
 		this.locations = locations;
+	}
+
+	public Location getInicio() {
+		return getLocation(inicio);
+	}
+
+	public void setInicio(String inicio) {
+		this.inicio = inicio;
+	}
+
+	public Location getLocation(String locationName) {
+		return locations.get(locationName);
 	}
 
 }
