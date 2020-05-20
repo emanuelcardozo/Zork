ab455df1b5da31efb291a36d19e8cbf80dfc2949
Emanuel Cardozo <leuname.01@gmail.com>
2020-05-20 20:08:29 -0300

    Separando las instrucciones de prueba

--- src/entities/App.java
+++ src/entities/App.java
@@ -7,34 +7,40 @@ public class App {
 		Aventura miAventura = new Aventura("escenario1.json");
 		Player jugador = miAventura.getJugador();
 
-		System.out.println("(VER INVENTARIO)");
+		System.out.println("\n(VER INVENTARIO)");
 		System.out.println(jugador.listarInventario());
 
-		System.out.println("--- Mira alrededor ---");
+		System.out.println("\n(MIRAR ALREDEDOR)");
 		System.out.println(jugador.mirar());
 
-		System.out.println("(AGARRAR BARRETA)");
+		System.out.println("\n(IR AL NORTE)");
+		System.out.println(jugador.moverHacia("norte"));
+
+		System.out.println("\n(IR AL SUR )");
+		System.out.println(jugador.moverHacia("sur"));
+
+		System.out.println("\n(AGARRAR BARRETA)");
 		System.out.println(jugador.agarrarItem("barreta"));
 
-		System.out.println("(MIRAR ALREDEDOR)");
+		System.out.println("\n(MIRAR ALREDEDOR)");
 		System.out.println(jugador.mirar());
 
-		System.out.println("(AGARRAR ESPEJO)");
+		System.out.println("\n(AGARRAR ESPEJO)");
 		System.out.println(jugador.agarrarItem("espejo"));
 
-		System.out.println("(AGARRAR BANANA)");
+		System.out.println("\n(AGARRAR BANANA)");
 		System.out.println(jugador.agarrarItem("banana"));
 
-		System.out.println("(MIRAR ALREDEDOR)");
+		System.out.println("\n(MIRAR ALREDEDOR)");
 		System.out.println(jugador.mirar());
 
-		System.out.println("(AGARRAR ROCIADOR)");
+		System.out.println("\n(AGARRAR ROCIADOR)");
 		System.out.println(jugador.agarrarItem("rociador con cerveza de raiz"));
 
-		System.out.println("(MIRAR ALREDEDOR)");
+		System.out.println("\n(MIRAR ALREDEDOR)");
 		System.out.println(jugador.mirar());
 
-		System.out.println("(IR AL SUR - ESTE - TABERNA)");
+		System.out.println("\n(IR AL SUR - TABERNA)");
 		System.out.println(jugador.moverHacia("sur"));
 
 	}
