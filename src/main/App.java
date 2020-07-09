6ee2230b20d3600cf0b0a20feca2ab3f38eba210
Emanuel Cardozo <leuname.01@gmail.com>
2020-07-09 16:44:25 -0300

    Agregando la ventana

--- src/main/App.java
+++ src/main/App.java
@@ -3,9 +3,11 @@ package main;
 import java.io.FileNotFoundException;
 
 import entities.Aventura;
+import visual.Ventana;
 
 public class App {
 	public static void main(String[] args) throws FileNotFoundException {
-		new Aventura();
+//		new Aventura();
+		new Ventana();
 	}
 }
