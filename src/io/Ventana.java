1ec2d6b955f9b8a1410c16c95f72180d852d63b8
Juan Pablo Del Greco <juanpablo.delgreco.jp@gmail.com>
2020-07-10 20:15:43 -0300

    Agregado de panel

--- src/io/Ventana.java
+++ src/io/Ventana.java
@@ -4,12 +4,11 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.io.FileNotFoundException;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -24,42 +23,42 @@ public class Ventana implements InOutputable {
 	private JTextField comandoTextInput;
 	private JTextArea inventarioTextArea;
 	private FileLogger fileLogger;
+	private JPanel panelImagen;
 
 	public Ventana() {
 		initialize();
 		ventanaFrame.setVisible(true);
 	}
-	
+
 	public void setText(String text) {
-		mainTextArea.append(text +"\n");
+		mainTextArea.append(text + "\n");
 	}
-	
+
 	public void setInventario(String item) {
-		inventarioTextArea.append(item +"\n");
+		inventarioTextArea.append(item + "\n");
 	}
-	
+
 	public String getInput() {
 		String text = null;
-		
+
 		try {
-			Thread.currentThread().wait();			
+			Thread.currentThread().wait();
 			text = comandoTextInput.getText();
 			comandoTextInput.setText("");
-			
+
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+
 		return text;
 	}
-	
 
 	private void initialize() {
 		Ventana self = this;
 		ventanaFrame = new JFrame();
-		ventanaFrame.setIconImage (new ImageIcon("./images/cobit-19.png").getImage());
+		ventanaFrame.setIconImage(new ImageIcon("./images/cobit-19.png").getImage());
 		ventanaFrame.setTitle("Zork COBIT-19");
-		ventanaFrame.setBounds(100, 100, 620, 420);
+		ventanaFrame.setBounds(100, 100, 650, 680);
 		ventanaFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		ventanaFrame.getContentPane().setLayout(null);
 		ventanaFrame.setLocationRelativeTo(null);
@@ -68,7 +67,7 @@ public class Ventana implements InOutputable {
 
 		JButton enviarButton = new JButton("Enviar");
 		enviarButton.setBackground(java.awt.Color.green);
-		enviarButton.setBounds(489, 337, 105, 35);
+		enviarButton.setBounds(510, 587, 105, 35);
 		ventanaFrame.getContentPane().add(enviarButton);
 
 		mainTextArea = new JTextArea();
@@ -77,44 +76,50 @@ public class Ventana implements InOutputable {
 		mainTextArea.setRows(15);
 		mainTextArea.setEditable(false);
 		JScrollPane scrollPane = new JScrollPane(mainTextArea);
-		scrollPane.setBounds(6, 6, 342, 296);
+		scrollPane.setBounds(20, 250, 342, 296);
 		mainTextArea.setAutoscrolls(true);
 		DefaultCaret caret = (DefaultCaret) mainTextArea.getCaret();
 		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
-		
+
 		ventanaFrame.getContentPane().add(scrollPane);
-		
+
+		panelImagen = new JPanel();
+		panelImagen.setBounds(20, 10, 593, 230);
+		panelImagen.setBackground(Color.ORANGE);
+		ventanaFrame.getContentPane().add(panelImagen);
+
 		comandoTextInput = new JTextField();
 		comandoTextInput.setToolTipText("Ingrese HELP para recibir ayuda.");
-		comandoTextInput.setBounds(6, 336, 477, 35);
-		comandoTextInput.addKeyListener(new KeyAdapter() {			
+		comandoTextInput.setBounds(20, 586, 477, 35);
+		comandoTextInput.addKeyListener(new KeyAdapter() {
 			public void keyPressed(KeyEvent e) {
 				synchronized (self) {
-				    if (e.getKeyCode()==KeyEvent.VK_ENTER){
-				    	self.notify();
-				    }
+					if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+						self.notify();
+					}
 				}
 			}
-	
+
 		});
+
 		ventanaFrame.getContentPane().add(comandoTextInput);
 		comandoTextInput.setColumns(100);
 
 		inventarioTextArea = new JTextArea();
 		inventarioTextArea.setEditable(false);
-		inventarioTextArea.setBounds(360, 59, 234, 243);
+		inventarioTextArea.setBounds(380, 302, 234, 243);
 		ventanaFrame.getContentPane().add(inventarioTextArea);
 
 		JLabel inventarioLabel = new JLabel("Inventario:");
 		inventarioLabel.setForeground(Color.white);
 		inventarioLabel.setFont(new Font("Serif", Font.PLAIN, 18));
-		inventarioLabel.setBounds(360, 33, 91, 16);
+		inventarioLabel.setBounds(380, 283, 91, 16);
 		ventanaFrame.getContentPane().add(inventarioLabel);
 
 		JLabel lblNewLabel_1 = new JLabel("Instruccion:");
 		lblNewLabel_1.setForeground(Color.white);
 		lblNewLabel_1.setFont(new Font("Serif", Font.PLAIN, 18));
-		lblNewLabel_1.setBounds(6, 314, 99, 16);
+		lblNewLabel_1.setBounds(20, 564, 99, 16);
 		ventanaFrame.getContentPane().add(lblNewLabel_1);
 		comandoTextInput.requestFocus();
 	}
